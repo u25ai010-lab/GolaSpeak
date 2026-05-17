56ef5d663a4336d8e1171a8f92bd1bf04d1b610a
u25ai010-lab <u25ai010@aid.svnit.ac.in>
2026-05-17 22:55:21 +0530

    Update CORS settings and Gola prompt examples

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["https://golaspeak.netlify.app"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
